cb2e779a9bf5ed88122fc5b59d39a3dc47356e0b
Javi <javsolgar@alum.us.es>
2020-04-06 20:44:05 +0200

    Fallo al crear la primera prueba

---
+++ src/test/java/org/springframework/samples/petclinic/view/HU_8.java
@@ -0,0 +1,97 @@
+
+package org.springframework.samples.petclinic.view;
+
+import java.util.concurrent.TimeUnit;
+
+import org.junit.Assert;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.Alert;
+import org.openqa.selenium.By;
+import org.openqa.selenium.NoAlertPresentException;
+import org.openqa.selenium.NoSuchElementException;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class HU_8 {
+
+	private WebDriver		driver;
+	private String			baseUrl;
+	private boolean			acceptNextAlert		= true;
+	private StringBuffer	verificationErrors	= new StringBuffer();
+
+
+	@BeforeEach
+	public void setUp() throws Exception {
+		String pathToGeckoDriver = System.getProperty("webdriver.gecko.driver");
+		System.setProperty("webdriver.gecko.driver", pathToGeckoDriver + "\\chromedriver.exe");
+		this.driver = new ChromeDriver();
+		this.baseUrl = "https://www.google.com/";
+		this.driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+	}
+
+	@Test
+	public void testAceptarAdopcion() throws Exception {
+		this.driver.get("http://localhost:8080/");
+		this.driver.findElement(By.linkText("Login")).click();
+		this.driver.findElement(By.id("username")).click();
+		this.driver.findElement(By.id("username")).clear();
+		this.driver.findElement(By.id("username")).sendKeys("shelter1");
+		this.driver.findElement(By.id("password")).click();
+		this.driver.findElement(By.id("password")).clear();
+		this.driver.findElement(By.id("password")).sendKeys("shelter1");
+		this.driver.findElement(By.xpath("//button[@type='submit']")).click();
+		this.driver.findElement(By.linkText("My animal shelter")).click();
+		this.driver.findElement(By.linkText("See applications")).click();
+		this.driver.findElement(By.linkText("See more")).click();
+		this.driver.findElement(By.linkText("Accept adoption")).click();
+		Assert.assertEquals("George Franklin", this.driver.findElement(By.xpath("//b")).getText());
+		Assert.assertEquals("Desto", this.driver.findElement(By.xpath("//dd")).getText());
+		this.driver.findElement(By.xpath("//div[@id='main-navbar']/ul[2]/li/a")).click();
+		this.driver.findElement(By.linkText("Logout")).click();
+		this.driver.findElement(By.xpath("//button[@type='submit']")).click();
+	}
+
+	@AfterEach
+	public void tearDown() throws Exception {
+		this.driver.quit();
+		String verificationErrorString = this.verificationErrors.toString();
+		if (!"".equals(verificationErrorString)) {
+			Assert.fail(verificationErrorString);
+		}
+	}
+
+	private boolean isElementPresent(final By by) {
+		try {
+			this.driver.findElement(by);
+			return true;
+		} catch (NoSuchElementException e) {
+			return false;
+		}
+	}
+
+	private boolean isAlertPresent() {
+		try {
+			this.driver.switchTo().alert();
+			return true;
+		} catch (NoAlertPresentException e) {
+			return false;
+		}
+	}
+
+	private String closeAlertAndGetItsText() {
+		try {
+			Alert alert = this.driver.switchTo().alert();
+			String alertText = alert.getText();
+			if (this.acceptNextAlert) {
+				alert.accept();
+			} else {
+				alert.dismiss();
+			}
+			return alertText;
+		} finally {
+			this.acceptNextAlert = true;
+		}
+	}
+}
