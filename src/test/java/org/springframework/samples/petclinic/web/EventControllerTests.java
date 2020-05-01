c0a75b229c0773cd5670d17e1599a476288084fd
rafsalcas1@alum.us.es <rafsalcas1@alum.us.es>
2020-05-01 13:59:04 +0200

    Test.

--- src/test/java/org/springframework/samples/petclinic/web/EventControllerTests.java
+++ src/test/java/org/springframework/samples/petclinic/web/EventControllerTests.java
@@ -1,28 +1,13 @@
 
 package org.springframework.samples.petclinic.web;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.FilterType;
 import org.springframework.samples.petclinic.configuration.SecurityConfiguration;
 import org.springframework.security.config.annotation.web.WebSecurityConfigurer;
-import org.springframework.security.test.context.support.WithMockUser;
-import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
-import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
 @WebMvcTest(controllers = EventController.class, excludeFilters = @ComponentScan.Filter(type = FilterType.ASSIGNABLE_TYPE, classes = WebSecurityConfigurer.class), excludeAutoConfiguration = SecurityConfiguration.class)
 public class EventControllerTests {
 
-	@Autowired
-	private MockMvc mockMvc;
-
-
-	@WithMockUser(value = "spring")
-	@Test
-	void testShowEventList() throws Exception {
-		this.mockMvc.perform(MockMvcRequestBuilders.get("/events/")).andExpect(MockMvcResultMatchers.status().isOk()).andExpect(MockMvcResultMatchers.model().attributeExists("events")).andExpect(MockMvcResultMatchers.view().name("events/eventsList"));
-	}
 }
