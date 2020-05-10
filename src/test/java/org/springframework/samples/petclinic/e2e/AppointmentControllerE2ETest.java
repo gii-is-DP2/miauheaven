d5c66752d6647da76e0a02f7eff9c9d963271882
sardongui <saradg1995@gmail.com>
2020-05-10 19:42:18 +0200

    Arreglos

--- src/test/java/org/springframework/samples/petclinic/e2e/AppointmentControllerE2ETest.java
+++ src/test/java/org/springframework/samples/petclinic/e2e/AppointmentControllerE2ETest.java
@@ -6,16 +6,6 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.samples.petclinic.model.Appointment;
-import org.springframework.samples.petclinic.model.Owner;
-import org.springframework.samples.petclinic.model.Pet;
-import org.springframework.samples.petclinic.model.Vet;
-import org.springframework.samples.petclinic.service.AppointmentService;
-import org.springframework.samples.petclinic.service.OwnerService;
-import org.springframework.samples.petclinic.service.PetService;
-import org.springframework.samples.petclinic.service.VetService;
-import org.springframework.samples.petclinic.web.AppointmentController;
 import org.springframework.samples.petclinic.web.OwnerController;
 import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors;
@@ -35,62 +25,27 @@ import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 @AutoConfigureMockMvc
 class AppointmentControllerE2ETest {
 
-	private static final int		TEST_APPOINTMENT_ID	= 1;
+	private static final int	TEST_OWNER_ID	= 6;
 
-	private static final int		TEST_OWNER_ID		= 6;
-
-	private static final int		TEST_PET_ID			= 7;
-
-	private static final int		TEST_VET_ID			= 1;
+	private static final int	TEST_PET_ID		= 7;
 
 	@Autowired
-	private AppointmentController	appointmentController;
-
-	@MockBean
-	private AppointmentService		appointmentService;
-
-	@MockBean
-	private OwnerService			ownerService;
-
-	@MockBean
-	private PetService				petService;
-
-	@MockBean
-	private VetService				vetService;
-
-	@Autowired
-	private MockMvc					mockMvc;
-
-	private Appointment				ap;
-
-	private Owner					ow;
-
-	private Pet						pet;
-
-	private Vet						vet;
+	private MockMvc				mockMvc;
 
 
-	@WithMockUser(username = "shelter1", authorities = {
-		"animalshelter"
-	})
+	@WithMockUser(value = "spring")
 	@Test
 	void testInitNewAppointmentForm() throws Exception {
 		this.mockMvc.perform(MockMvcRequestBuilders.get("/owners/*/pets/{petId}/appointment/new", AppointmentControllerE2ETest.TEST_PET_ID)).andExpect(MockMvcResultMatchers.status().isOk())
 			.andExpect(MockMvcResultMatchers.model().attributeExists("appointment")).andExpect(MockMvcResultMatchers.view().name("appointment/createOrUpdateAppointmentForm"));
 	}
-	@WithMockUser(username = "shelter1", authorities = {
-		"animalshelter"
-	})
-
+	@WithMockUser(value = "spring")
 	@Test
 	void testProcessCreationFormSuccess() throws Exception {
-		this.mockMvc.perform(MockMvcRequestBuilders.post("/owners/{ownerId}/pets/{petId}/appointment/new", AppointmentControllerE2ETest.TEST_OWNER_ID, AppointmentControllerE2ETest.TEST_PET_ID).param("cause", "No come nada")
+		this.mockMvc.perform(MockMvcRequestBuilders.post("/owners/{ownerId}/pets/{petId}/appointment/new", AppointmentControllerE2ETest.TEST_OWNER_ID, AppointmentControllerE2ETest.TEST_PET_ID).param("cause", "Tiene rota la patita")
 			.with(SecurityMockMvcRequestPostProcessors.csrf()).param("urgent", "true")).andExpect(MockMvcResultMatchers.status().is3xxRedirection());
 	}
-
-	@WithMockUser(username = "shelter1", authorities = {
-		"animalshelter"
-	})
+	@WithMockUser(value = "spring")
 	@Test
 	void testProcessCreationFormHasErrors() throws Exception {
 		this.mockMvc
