de81817f0cc89491e25ba4cb1a4c117172b1c0b8
Javi <javsolgar@alum.us.es>
2020-05-11 19:24:50 +0200

    Prueba de rendimiento 20 Closes #133

---
+++ performanceTest/rendimiento20.scala
@@ -0,0 +1,74 @@
+package dp2
+
+import scala.concurrent.duration._
+
+import io.gatling.core.Predef._
+import io.gatling.http.Predef._
+import io.gatling.jdbc.Predef._
+
+class rendimiento20 extends Simulation {
+
+	val httpProtocol = http
+		.baseUrl("http://www.dp2.com")
+		.inferHtmlResources(BlackList(""".*.css""", """.*.ico""", """.*.jpg""", """.*.png""", """.*.js"""), WhiteList())
+		.acceptHeader("text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9")
+		.acceptEncodingHeader("gzip, deflate")
+		.acceptLanguageHeader("en-US,en;q=0.9,es;q=0.8")
+		.userAgentHeader("Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/81.0.4044.138 Safari/537.36")
+
+	val headers_0 = Map(
+		"Proxy-Connection" -> "keep-alive",
+		"Upgrade-Insecure-Requests" -> "1")
+
+	val headers_2 = Map(
+		"Accept" -> "image/webp,image/apng,image/*,*/*;q=0.8",
+		"Proxy-Connection" -> "keep-alive")
+
+	val headers_3 = Map(
+		"Origin" -> "http://www.dp2.com",
+		"Proxy-Connection" -> "keep-alive",
+		"Upgrade-Insecure-Requests" -> "1")
+
+
+
+	val scn = scenario("Home")
+		.exec(http("request_0")
+			.get("/")
+			.headers(headers_0))
+		.pause(7)
+		// Home
+		.exec(http("Login")
+			.get("/login")
+			.headers(headers_0)
+			.resources(http("request_2")
+			.get("/favicon.ico")
+			.headers(headers_2)))
+		.pause(10)
+		// Login
+		.exec(http("logged")
+			.post("/login")
+			.headers(headers_3)
+			.formParam("username", "admin1")
+			.formParam("password", "4dm1n")
+			.formParam("_csrf", "d6ad887f-381f-4313-88dc-8226630dff4f"))
+		.pause(6)
+		// logged
+		.exec(http("See appointments")
+			.get("/admin/appointments")
+			.headers(headers_0))
+		.pause(11)
+		// See appointments
+		.exec(http("See appointment details")
+			.get("/admin/appointments/1")
+			.headers(headers_0))
+		.pause(10)
+		// See appointment details
+
+	setUp(scn.inject(rampUsers(16500) during (100 seconds)))
+	.protocols(httpProtocol)
+	.assertions(
+		global.responseTime.max.lt(5000),
+		global.responseTime.mean.lt(1000),
+		global.successfulRequests.percent.gt(95)
+	)
+}
