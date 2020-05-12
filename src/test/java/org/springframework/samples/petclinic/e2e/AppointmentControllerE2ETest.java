54b1ac5702417c4418c3850e11f9bc27c9d774eb
fragonlug <Usuario@DESKTOP-JDQOE0R>
2020-05-12 10:39:19 +0200

    Arreglado mis purebas de Interfaz 6,7,14,16,17 junto a la creacion de las pruebas de las 28

---
+++ src/test/java/org/springframework/samples/petclinic/e2e/AppointmentControllerE2ETest.java
@@ -0,0 +1,63 @@
+
+package org.springframework.samples.petclinic.e2e;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.samples.petclinic.web.OwnerController;
+import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
+/**
+ * Test class for {@link OwnerController}
+ *
+ * @author Colin But
+ */
+
+@ExtendWith(SpringExtension.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.MOCK)
+@AutoConfigureMockMvc
+class AppointmentControllerE2ETest {
+
+	private static final int	TEST_OWNER_ID	= 6;
+
+	private static final int	TEST_PET_ID		= 7;
+
+	@Autowired
+	private MockMvc				mockMvc;
+
+
+	@WithMockUser(username = "owner1", authorities = {
+		"owner"
+	})
+	@Test
+	void testInitNewAppointmentForm() throws Exception {
+		this.mockMvc.perform(MockMvcRequestBuilders.get("/owners/*/pets/{petId}/appointment/new", AppointmentControllerE2ETest.TEST_PET_ID)).andExpect(MockMvcResultMatchers.status().isOk())
+			.andExpect(MockMvcResultMatchers.model().attributeExists("appointment")).andExpect(MockMvcResultMatchers.view().name("appointment/createOrUpdateAppointmentForm"));
+	}
+	@WithMockUser(username = "owner1", authorities = {
+		"owner"
+	})
+	@Test
+	void testProcessCreationFormSuccess() throws Exception {
+		this.mockMvc.perform(MockMvcRequestBuilders.post("/owners/{ownerId}/pets/{petId}/appointment/new", AppointmentControllerE2ETest.TEST_OWNER_ID, AppointmentControllerE2ETest.TEST_PET_ID).param("cause", "No come nada")
+			.with(SecurityMockMvcRequestPostProcessors.csrf()).param("urgent", "true").param("date", "2020/12/12").param("vet_id", "1")).andExpect(MockMvcResultMatchers.status().is3xxRedirection());
+	}
+	@WithMockUser(username = "owner1", authorities = {
+		"owner"
+	})
+	@Test
+	void testProcessCreationFormHasErrors() throws Exception {
+		this.mockMvc
+			.perform(
+				MockMvcRequestBuilders.post("/owners/{ownerId}/pets/{petId}/appointment/new", AppointmentControllerE2ETest.TEST_OWNER_ID, AppointmentControllerE2ETest.TEST_PET_ID).with(SecurityMockMvcRequestPostProcessors.csrf()).param("urgent", "null"))
+			.andExpect(MockMvcResultMatchers.status().isOk()).andExpect(MockMvcResultMatchers.model().attributeHasErrors("appointment")).andExpect(MockMvcResultMatchers.model().attributeHasFieldErrors("appointment", "cause"))
+			.andExpect(MockMvcResultMatchers.view().name("appointment/createOrUpdateAppointmentForm"));
+	}
+}
