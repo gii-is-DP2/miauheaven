bab3cca56ecc9de5b92eed2a4c19c989d6ba6616
rafsalcas1 <rafsalcas1@alum.us.es>
2020-05-30 20:23:14 +0200

    Añadido la prueba de crear un producto

--- src/test/java/org/springframework/samples/petclinic/service/ProductServiceTests.java
+++ src/test/java/org/springframework/samples/petclinic/service/ProductServiceTests.java
@@ -3,6 +3,8 @@ package org.springframework.samples.petclinic.service;
 
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
+import java.util.Collection;
+
 import javax.validation.ConstraintViolationException;
 
 import org.assertj.core.api.Assertions;
@@ -12,6 +14,7 @@ import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabas
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.samples.petclinic.model.Product;
+import org.springframework.samples.petclinic.repository.ProductRepository;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -20,8 +23,48 @@ import org.springframework.transaction.annotation.Transactional;
 public class ProductServiceTests {
 
 	@Autowired
-	private ProductService productService;
+	private ProductService		productService;
 
+	@Autowired
+	private ProductRepository	productRepository;
+
+
+	// ---------------------------------------------------------------- HU.27 ----------------------------------------------------------------------------------------------------
+
+	@Test
+	@Transactional
+	public void shouldInsertProduct() {
+		Collection<Product> products = this.productService.findAll();
+		Product p = this.productService.findProductById(1);
+
+		final int found = products.size();
+
+		final Product product = new Product();
+		product.setDescription("Prueba");
+		product.setName("Producto1");
+		product.setImage("https://www.petpremium.com/wp-content/uploads/2012/10/5-healthy-dog-foods-430x226.jpg");
+		product.setPrice(20.0);
+		product.setStock(true);
+		this.productService.save(product);
+		Assertions.assertThat(product.getId().longValue()).isNotEqualTo(0);
+
+		products = this.productService.findAll();
+		Assertions.assertThat(products.size()).isEqualTo(found + 1);
+		this.productRepository.deleteById(product.getId());
+	}
+
+	@Test
+	@Transactional
+	public void shouldNotInsertProduct() {
+		final Product product = new Product();
+		product.setDescription("Prueba");
+		product.setName("Prueba");
+
+		assertThrows(ConstraintViolationException.class, () -> {
+			this.productService.save(product);
+		});
+		//	this.eventRepository.deleteById(event.getId());
+	}
 
 	// ---------------------------------------------------------------- HU.28 ----------------------------------------------------------------------------------------------------
 
@@ -49,7 +92,7 @@ public class ProductServiceTests {
 		String newName = "";
 		Double newPrice = null;
 		Boolean newStock = null;
-		
+
 		assertThrows(ConstraintViolationException.class, () -> {
 			Product product = this.productService.findProductById(1);
 			product.setName(newName);
